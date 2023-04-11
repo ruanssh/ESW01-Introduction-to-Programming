580ad7ffcd97a5cc4b73b52ccdf25efd1198b028
ruanssh <gamesplay710@gmail.com>
2023-04-11 02:39:12 -0400

    quest 14, 10, 6, 7

---
+++ Exercicios/10/Exercicio-10.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	printf("Imprimir numeros de -1 a 300:\n");
+    int num = -100;
+    while (num <= 300) {
+        printf("Numero: %d\n", num);
+        num++;
+    }
+    return 0;
+}
